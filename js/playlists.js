57763660b77c95af2931e4f453703dc2f30c26cb
Niccolò Albanese <nick_albanese@virgilio.it>
2024-11-19 18:04:58 +0100

    Create playlists.js

---
+++ js/playlists.js
@@ -0,0 +1,64 @@
+const playlistsArray = [
+  {
+    playlistName: "IslandPop",
+    playlistSpotify: true,
+    playlistTracks: [1, 2, 3, 4, 5, 6],
+  },
+  {
+    playlistName: "sleep",
+    playlistSpotify: true,
+    playlistTracks: [1, 2, 3, 4, 5, 6],
+  },
+  {
+    playlistName: "stressRelief",
+    playlistSpotify: true,
+    playlistTracks: [1, 2, 3, 4, 5, 6],
+  },
+  {
+    playlistName: "soulLounge",
+    playlistSpotify: true,
+    playlistTracks: [1, 2, 3, 4, 5, 6],
+  },
+  {
+    playlistName: "deepFocus",
+    playlistSpotify: true,
+    playlistTracks: [1, 2, 3, 4, 5, 6],
+  },
+  {
+    playlistName: "peacefullGuitar",
+    playlistSpotify: true,
+    playlistTracks: [1, 2, 3, 4, 5, 6],
+  },
+  {
+    playlistName: "capodanno 2025",
+    playlistSpotify: false,
+    playlistTracks: [1, 2, 3, 4, 5, 6],
+  },
+  {
+    playlistName: "estate 2024",
+    playlistSpotify: false,
+    playlistTracks: [1, 2, 3, 4, 5, 6],
+  },
+  {
+    playlistName: "lollo crazy",
+    playlistSpotify: false,
+    playlistTracks: [1, 2, 3, 4, 5, 6],
+  },
+  {
+    playlistName: "orsi polari",
+    playlistSpotify: false,
+    playlistTracks: [1, 2, 3, 4, 5, 6],
+  },
+  {
+    playlistName: "daje forte",
+    playlistSpotify: false,
+    playlistTracks: [1, 2, 3, 4, 5, 6],
+  },
+  {
+    playlistName: "Epi-fucking",
+    playlistSpotify: false,
+    playlistTracks: [1, 2, 3, 4, 5, 6],
+  },
+
+
+];

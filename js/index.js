9295d3ca802c0fb882e9ff1d4b3468b5d01ae090
Lorenz <l.lione@remoteitalia.com>
2024-11-21 12:19:55 +0100

    Integrazione leftColumn in index.html

--- js/index.js
+++ js/index.js
@@ -4,6 +4,78 @@
 // ***********************************************************************
 //
 
+const populateMyPlaylist = async (list) => {
+    _W(`populateMyPlaylist - list: `, list)
+    playlistsMegaArray.length = 0
+    const data = await Promise.all(
+        list.map((id) => {
+            let usingURL = TRACK_URL + id
+            return fetchFunction(usingURL)
+        })
+    )
+    playlistsMegaArray.push(...data)
+
+    _D(3, playlistsMegaArray,)
+}
+
+
+const populateMyLibrary = async () => {
+    libraryArray.length = 0
+    const data = await Promise.all(
+        myLibraryID.map((id) => {
+            let usingURL = ALBUM_URL + id
+            return fetchFunction(usingURL)
+        })
+    )
+    libraryArray.push(...data)
+
+    _D(3, libraryArray,)
+}
+
+
+const drawPlaylistsLinks = () => {
+    let playListsLiHTML = ''
+    playlistsArray
+        .filter((playlist) => playlist.playlistSpotify === false)
+        .forEach((playlist) => {
+
+            playListsLiHTML += `
+            <li>
+                <p id="playList-${playlist.playlistName.replace(' ', '')}" class="btn p-0 me-2 text-body-secondary fw-normal text-truncate">
+                    ${playlist.playlistName}
+                </p>
+            </li>
+        `})
+
+    document.getElementById('playlists-list').innerHTML = playListsLiHTML
+}
+
+
+const drawOurLibrary = (albumsArray) => {
+    let playlistCardsHTML = ''
+    albumsArray.forEach((album) => {
+        playlistCardsHTML += `
+        <div class="col">
+          <div id="album-${album.id}" class="card bg-body-tertiary rounded">
+            <div class="card-body d-flex align-items-center">
+              <img id="albumImage-${album.id}"
+                src="${album.cover}"
+                class="me-3 rounded"
+                alt="Album Cover"
+                style="width: 70px; height: 70px; object-fit: cover;"
+              />
+              <div>
+                <h5 id="albumTitle-${album.id}" class="card-title mb-0">${album.artist.name}</h5>
+                <p id="albumText-${album.id}" class="card-text text-muted">${album.title}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        `
+    })
+    document.getElementById('ourPlayListsSection').innerHTML = playlistCardsHTML
+}
+
 
 
 //
@@ -14,6 +86,32 @@
 // ***********************************************************************
 //
 
+// Targhettizzo l'informazione inserita nel form per la ricerca
+const queryInput = document.getElementById('query')
+
+// Targhettizzo il form per prevenire anche la gestione di default del submit che mi resetterebbe i campi
+const form = document.getElementById('columnForm')
+
+//   ed inizializzo l'array fuori dalla funzione di ricerca
+const queryArray = []
+
+// metto l'URL all'interno di una costante per poter essere più facilmente utilizzato
+const SEARCH_URL =
+    'https://striveschool-api.herokuapp.com/api/deezer/search?q='
+
+// qui gestisco la mia libreria
+const myLibraryID = [
+    726319, 1236002, 119282, 1316047, 1318764, 113578, 59853992, 401032,
+    9884672, 116670,
+]
+
+
+
+//   targhettizzo il link nella colonna sinistra che mi possa condividere l'array di album
+let libraryArray = []
+let playlistsMegaArray = []
+const ALBUM_URL = 'https://striveschool-api.herokuapp.com/api/deezer/album/'
+const TRACK_URL = 'https://striveschool-api.herokuapp.com/api/deezer/track/'
 
 
 //
@@ -38,6 +136,7 @@ document.addEventListener('DOMContentLoaded', async () => {
     document.getElementById('rightColumn').innerHTML = rightColumnHTML.match(/<main[^>]*>([\s\S]*?)<\/main>/i)[1];
     document.getElementsByTagName('footer')[0].innerHTML = navbarHTML.match(/<footer[^>]*>([\s\S]*?)<\/footer>/i)[1];
 
+    drawPlaylistsLinks()
 
     document.getElementsByTagName('body')[0].addEventListener('click', (e) => {
         const target = e.target.id
@@ -75,6 +174,10 @@ document.addEventListener('DOMContentLoaded', async () => {
             }
 
             case 'playList': {
+                const tracksPlaylistArray =
+                    playlistsArray.
+                        filter((playlist) => playlist.playlistName.replace(' ', '') === targetObject)[0].playlistTracks
+                populateMyPlaylist(tracksPlaylistArray)
                 drawPlaylist(targetObject)
                 break
             }
@@ -100,6 +203,15 @@ document.addEventListener('DOMContentLoaded', async () => {
                     drawArtist(targetObject)
                     break
                 }
+
+            case 'libraryLink': {
+                document.getElementById('switchTitle').innerText = 'La tua libreria'
+                document.getElementById('ourPlayListsSection').innerHTML = ''
+                populateMyLibrary().then(() => {
+                    console.log('libraryArray:', libraryArray)
+                    drawOurLibrary(libraryArray)
+                })
+            }
 
             default: { break }
         }
